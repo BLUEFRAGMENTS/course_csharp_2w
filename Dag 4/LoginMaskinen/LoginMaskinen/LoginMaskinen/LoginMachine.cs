d0e5de834d05950980dca271bc832a5c9fe7ce4e
Mathias Kirkegaard <matn@bluefragments.com>
2022-05-05 10:50:55 +0200

    Ændringer til loginmaskine

---
+++ Dag 4/LoginMaskinen/LoginMaskinen/LoginMaskinen/LoginMachine.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace LoginMaskinenNamespace
+{
+    public class LoginMachine
+    {
+        private string? _username;
+        private string? _password;
+
+        public string? Username
+        {
+            get
+            {
+                return _username;
+            }
+
+            set
+            {
+                _username = value;
+                if (IsUsernameValid == false)
+                {
+                    _username = string.Empty;
+                    Console.WriteLine("Brugernavn duer ikke. Prøv igen.");
+                }
+            }
+        }
+
+        public string? Password
+        {
+            get
+            {
+                return _password;
+            }
+
+            set
+            {
+                _password = value;
+                if (IsPasswordValid == false)
+                {
+                    _password = string.Empty;
+                    Console.WriteLine("Password duer ikke. Prøv igen.");
+                }
+            }
+        }
+
+        public bool IsUsernameValid
+        {
+            get { return Username?.Length > 0 && Username?.Length < 8; }
+        }
+
+        public bool IsPasswordValid
+        {
+            get
+            {
+                if (_password != null)
+                {
+                    Regex regex = new Regex(@"^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$");
+                    bool hasNumberAndLetter = regex.IsMatch(_password);
+                    return
+                        _password?.Length > 0
+                        && _password?.Length < 8
+                        && hasNumberAndLetter;
+                }
+
+                return false;
+            }
+        }
+
+        public void TryToLogin()
+        {
+            if (IsUsernameValid && IsPasswordValid)
+            {
+                Console.WriteLine("Du er nu logget ind");
+            }
+            else
+            {
+                Console.WriteLine("Du kunne ikke logge ind");
+            }
+        }
+    }
+}
--- Dag 4/LoginMaskinen/LoginMaskinen/LoginMaskinen/Program.cs
+++ Dag 4/LoginMaskinen/LoginMaskinen/LoginMaskinen/Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Text.RegularExpressions;
+﻿using LoginMaskinenNamespace;
 
 // Step 1
 Console.WriteLine("LOGINMASKINEN");
@@ -27,82 +27,4 @@ while (loginMachine.IsPasswordValid == false)
 // Step 7
 Console.WriteLine();
 loginMachine.TryToLogin();
-
-
-public class LoginMachine
-{
-    private string? _username;
-    private string? _password;
-
-    public string? Username
-    {
-        get
-        {
-            return _username;
-        }
-
-        set
-        {
-            _username = value;
-            if (IsUsernameValid == false)
-            {
-                _username = string.Empty;
-                Console.WriteLine("Brugernavn duer ikke. Prøv igen.");
-            }
-        }
-    }
-
-    public string? Password
-    {
-        get
-        {
-            return _password;
-        }
-
-        set
-        {
-            _password = value;
-            if (IsPasswordValid == false)
-            {
-                _password = string.Empty;
-                Console.WriteLine("Password duer ikke. Prøv igen.");
-            }
-        }
-    }
-
-    public bool IsUsernameValid
-    {
-        get { return Username?.Length > 0 && Username?.Length < 8; }
-    }
-
-    public bool IsPasswordValid
-    {
-        get
-        {
-            if (_password != null)
-            {
-                Regex regex = new Regex(@"^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$");
-                bool hasNumberAndLetter = regex.IsMatch(_password);
-                return
-                    _password?.Length > 0
-                    && _password?.Length < 8
-                    && hasNumberAndLetter;
-            }
-
-            return false;
-        }
-    }
-
-    public void TryToLogin()
-    {
-        if (IsUsernameValid && IsPasswordValid)
-        {
-            Console.WriteLine("Du er nu logget ind");
-        }
-        else
-        {
-            Console.WriteLine("Du kunne ikke logge ind");
-        }
-    }
-}
 
