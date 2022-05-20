847762f13d4d5da48c271a7f4449bf3f7c7b39e1
Mathias Kirkegaard <matn@bluefragments.com>
2022-05-20 10:11:30 +0200

    Change language more test

--- Eksempler/Forms eksempler/CourseExampleLibrary/CourseExampleLibrary/CourseExampleLibrary.UWP/Services/LanguageService.cs
+++ Eksempler/Forms eksempler/CourseExampleLibrary/CourseExampleLibrary/CourseExampleLibrary.UWP/Services/LanguageService.cs
@@ -9,8 +9,11 @@ namespace CourseExampleLibrary.UWP.Services
     {
         public void ChangeLanguage(string language)
         {
+            // Tror ikke dette er nødvendigt for UWP (men nok for andre platforme)
             Thread.CurrentThread.CurrentCulture = new CultureInfo(language);
             Thread.CurrentThread.CurrentUICulture = new CultureInfo(language);
+
+            // Men det her er...
             ApplicationLanguages.PrimaryLanguageOverride = language;
             Windows.ApplicationModel.Resources.Core.ResourceContext.GetForCurrentView().Reset();
             Windows.ApplicationModel.Resources.Core.ResourceContext.GetForViewIndependentUse().Reset();
