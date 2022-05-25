c765a075359bf05dde9c3448ff8d75a08aeb15ee
Mathias Kirkegaard <matn@bluefragments.com>
2022-05-25 10:12:59 +0200

    Adding a test project

---
+++ Eksempler/Forms eksempler/CourseExampleLibrary/CourseExampleLibrary.Tests/Utilities/CalculatorTests.cs
@@ -0,0 +1,53 @@
+﻿using CourseExampleLibrary.Utilities;
+
+namespace CourseExampleLibrary.Tests.Utilities
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void Add_2Plus2_Returns4()
+        {
+            // ARRANGE
+            // sut = SubjectUnderTest
+            var sut = new Calculator();
+            const double expectedResult = 4;
+
+            // ACT
+            var result = sut.Add(2, 2);
+
+            // ASSERT
+            Assert.IsTrue(result == expectedResult);
+        }
+
+        [TestMethod]
+        public void Add_2PlusMinus4_ReturnsMinus2()
+        {
+            // ARRANGE
+            // sut = SubjectUnderTest
+            var sut = new Calculator();
+            const double expectedResult = -2;
+
+            // ACT
+            var result = sut.Add(2, -4);
+
+            // ASSERT
+            Assert.IsTrue(result == expectedResult);
+        }
+
+        [TestMethod]
+        public void Add_3MultipliedWith3_Returns9()
+        {
+            // ARRANGE
+            // sut = SubjectUnderTest
+            var sut = new Calculator();
+            const double expectedResult = 9;
+
+            // ACT
+            var result = sut.Multiply(3, 3);
+
+            // ASSERT
+            Assert.IsTrue(result == expectedResult, "Multiply 3 with 3 did not return 9");
+        }
+    }
+}
---
+++ Eksempler/Forms eksempler/CourseExampleLibrary/CourseExampleLibrary/CourseExampleLibrary/Utilities/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CourseExampleLibrary.Utilities
+{
+    public class Calculator
+    {
+        public double Add(double number1, double number2)
+        {
+            return number1 + number2;
+        }
+
+        public double Minus(double minusFrom, double minusWith)
+        {
+            return minusFrom - minusWith;
+        }
+
+        public double Multiply(double number1, double number2)
+        {
+            return number1 * number2 + 1;
+        }
+    }
+}
